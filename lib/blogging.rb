ab642482ee3a914314a0cf05ef45b058fb693f58
Jez Cope <j.cope@erambler.co.uk>
2011-08-13 21:59:38 +0100

    Remove unused excerpt helpers.

--- lib/blogging.rb
+++ lib/blogging.rb
@@ -1,18 +1,7 @@
-require 'nokogiri'
-
 include Nanoc3::Helpers::Blogging
 
 def date_for(article)
   attribute_to_time article[:created_at]
-end
-
-def excerpt_for(article, pars = 1)
-	article[:excerpt] || generate_excerpt(article, pars)
-end
-
-def generate_excerpt(article, pars = 1)
-	doc = Nokogiri::HTML::DocumentFragment.parse(article.compiled_content)
-	doc.css('p')[0,pars].to_html
 end
 
 def paginate_articles

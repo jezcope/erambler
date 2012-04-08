3d538e056671acad9834484d5034f2565093fb1f
Jez Cope <j.cope@erambler.co.uk>
2012-04-08 19:04:09 +0100

    Move tag_feed_for into new TagIndex class.

--- lib/tagging.rb
+++ lib/tagging.rb
@@ -1,7 +1,35 @@
-def tag_feed_for(tag)
-  slug = tag.downcase.gsub(/[^a-z0-9]/, '-')
-  Nanoc3::Item.new(
-    "= render 'partials/tag_feed', :tag => '#{tag}'",
-    {},
-    "/feeds/tag/#{slug}")
+def tag_index
+  TagIndex.tag_index
+end
+
+class TagIndex
+
+  def TagIndex.tag_index
+    @@instance ||= TagIndex.new
+  end
+
+  def add_articles(items)
+    @tags = Hash.new()
+    puts 'TagIndex.initialize'
+
+    items.each do |i|
+      i[:tags].andand.each do |tag|
+        @tags[tag] ||= []
+        @tags[tag] << i
+      end
+    end
+  end
+
+  def items_for_tag(tag)
+    @tags[tag]
+  end
+
+  def tag_feed_for(tag)
+    slug = tag.downcase.gsub(/[^a-z0-9]/, '-')
+    Nanoc3::Item.new(
+      "= render 'partials/tag_feed', :tag => '#{tag}'",
+      {},
+      "/feeds/tag/#{slug}")
+  end
+
 end

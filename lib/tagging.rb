0c06e6df64c33dbcb573e8c14349a206dfd99ee4
Jez Cope <j.cope@erambler.co.uk>
2012-04-09 08:42:26 +0100

    Auto-generate feeds for all tags.

--- lib/tagging.rb
+++ lib/tagging.rb
@@ -10,7 +10,6 @@ class TagIndex
 
   def add_articles(items)
     @tags = Hash.new()
-    puts 'TagIndex.initialize'
 
     items.each do |i|
       i[:tags].andand.each do |tag|
@@ -24,10 +23,14 @@ class TagIndex
     @tags[tag]
   end
 
+  def tag_feeds
+    @tags.map { |t, _| tag_feed_for t }
+  end
+
   def tag_feed_for(tag)
     slug = tag.downcase.gsub(/[^a-z0-9]/, '-')
     Nanoc3::Item.new(
-      "= render 'partials/tag_feed', :tag => '#{tag}'",
+      "= render 'partials/tag_feed', :tag => #{tag.inspect}",
       {},
       "/feeds/tag/#{slug}")
   end

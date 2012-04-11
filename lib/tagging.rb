23052578095367c71f56546c2dd40be3cb03bf25
Jez Cope <j.cope@erambler.co.uk>
2012-04-11 21:10:30 +0100

    Tidy up and refactor slug code.

--- lib/tagging.rb
+++ lib/tagging.rb
@@ -8,12 +8,15 @@ class TagIndex
     @@instance ||= TagIndex.new
   end
 
+  def slug_for(tag)
+    tag.downcase.gsub(/[^a-z0-9]/, '-')
+  end
+
   def add_articles(items)
-    @tags = Hash.new()
+    @tags = Hash.new{|hash, key| hash[key] = []}
 
     items.each do |i|
       i[:tags].andand.each do |tag|
-        @tags[tag] ||= []
         @tags[tag] << i
       end
     end
@@ -24,15 +27,15 @@ class TagIndex
   end
 
   def tag_feeds
-    @tags.map { |t, _| tag_feed_for t }
+    @tags.keys.map { |t| tag_feed_for t }
   end
 
   def tag_feed_for(tag)
-    slug = tag.downcase.gsub(/[^a-z0-9]/, '-')
     Nanoc3::Item.new(
       "= render 'partials/feed', :articles => tag_index.items_for_tag(#{tag.inspect})",
       {},
-      "/feeds/tag/#{slug}")
+      "/feeds/tag/#{slug_for tag}")
+  end
   end
 
 end

c6150af29eb79ea8d997c6f146ef4ff293ebe791
Jez Cope <j.cope@erambler.co.uk>
2013-03-04 17:45:06 +0000

    Cache partials that don't change to speed up compilation.

--- lib/default.rb
+++ lib/default.rb
@@ -4,3 +4,9 @@ require 'faker'
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::XMLSitemap
+
+def render_cached(partial, *args)
+  $cache ||= {}
+
+  $cache[partial] ||= render(partial, *args)
+end

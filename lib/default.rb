e10f783757449cb841dcd9aa9539eb2755c3a31a
Jez Cope <j.cope@erambler.co.uk>
2016-03-29 16:30:45 +0100

    Remove caching (let nanoc handle that instead)

--- lib/default.rb
+++ lib/default.rb
@@ -5,13 +5,3 @@ require 'nokogiri'
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::XMLSitemap
-
-def render_cached(partial, *args)
-  $cache ||= {}
-
-  $cache[partial] ||= render(partial, *args)
-end
-
-def reset_cache
-  $cache = {}
-end

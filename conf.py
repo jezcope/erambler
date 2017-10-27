6045b072d35fb05728b39a00f7d4f52aeebc14a2
Jez Cope <j.s.cope@sheffield.ac.uk>
2017-10-27 21:24:14 +0100

    Add last 2017 posts

--- conf.py
+++ conf.py
@@ -271,6 +271,7 @@ FILES_FOLDERS = {
 # 'html' assumes the file is HTML and just copies it
 COMPILERS = {
     "markdown": ('.md', '.mdown', '.markdown'),
+    "pandoc": ('.org',),
     "rest": ('.rst', '.txt'),
     "textile": ('.textile',),
     "txt2tags": ('.t2t',),
@@ -281,7 +282,6 @@ COMPILERS = {
     # Pandoc detects the input from the source filename
     # but is disabled by default as it would conflict
     # with many of the others.
-    "pandoc": ('.org'),
     # PHP files are rendered the usual way (i.e. with the full templates).
     # The resulting files have .php extensions, making it possible to run
     # them without reconfiguring your server to recognize them.

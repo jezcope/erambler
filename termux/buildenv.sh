5f3728ce367180eafbf58697c033b3a60c1c5f8e
Jez Cope <j.cope@erambler.co.uk>
2019-11-24 17:31:06 +0000

    Update to work under termux on android

---
+++ termux/buildenv.sh
@@ -0,0 +1,1 @@
+export CPPFLAGS="-I /data/data/com.termux/files/usr/include/libxml2"
---
+++ termux/deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt install unstable-repo # for pandoc
+apt update
+
+apt install libxml2 libxslt
+apt install zlib libjpeg-turbo
+apt install libzmq
+apt install pandoc
+
+apt install nodejs-lts
+npm install -g less
+

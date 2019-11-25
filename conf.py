160cebd0be9f6816f334f09a716af707a2ddd971
Jez Cope <j.cope@erambler.co.uk>
2019-11-25 20:06:03 +0000

    Remove Google Analytics tracking code

--- conf.py
+++ conf.py
@@ -1133,18 +1133,7 @@ MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'extra', 'admonition']
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
-GOOGLE_ANALYTICS = "UA-10201101-1"
-BODY_END = f"""
-<script>
-  (function(i,s,o,g,r,a,m){{i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){{
-  (i[r].q=i[r].q||[]).push(arguments)}},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  }})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', '{GOOGLE_ANALYTICS}', 'auto');
-  ga('send', 'pageview');
-</script>
-"""
+# BODY_END = ""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
